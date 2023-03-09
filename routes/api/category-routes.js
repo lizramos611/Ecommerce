93755a55b0e562e9b337d1fec9df2b99b3e76d6e
lizramos611 <liz.ramos@aim.com>
2023-03-09 13:23:49 -0500

    trying to find routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,21 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findAll({
+        inclde {model: Product};
+        res.status(200).jason(categoryData)
+    }catch(error) {
+        res.status(500).json(error)
+    }
+    )
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
