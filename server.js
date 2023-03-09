9b301ff03f046db6a4a15b04b189d4d9d33983f7
lizramos611 <liz.ramos@aim.com>
2023-03-09 13:13:30 -0500

    added comments and starter code

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const routes = require('./routes');
 const express = require('express');
-const sequalize = require('./config/connection');
+const routes = require('./routes');
+// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express/JSON());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
 
-
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
